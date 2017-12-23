1ac3fa1b08863406737894b8302283ca00006033
td1tfx <td1tfx@163.com>
2017-12-23 10:31:08 +0900

    finish, over

--- DP-demo/DP-demo.cpp
+++ DP-demo/DP-demo.cpp
@@ -18,22 +18,24 @@ int main()
 	//op.gradientDescent(-5, 0.2, Function::func1, Function::grad1, 20, 0.0001,0.0);
 	srand((unsigned)time(NULL));
 
-	int in_num = 3;
-	int out_num = 2;
+	int in_num = 2;
+	int out_num = 1;
 	int hidden_num = 4;
-	int batch_num = 3;
+	int batch_num = 4;
 	double lr = 0.1;
 	FullConnection fc(in_num, hidden_num, batch_num,lr);
 	FullConnection fc1(hidden_num, hidden_num, batch_num,lr);
 	FullConnection fc2(hidden_num, out_num, batch_num,lr);
 	double** in_data = new double*[batch_num];
-	in_data[0] = new double[in_num] {3, 5, 8};
-	in_data[1] = new double[in_num] {2, 4, 3};
-	in_data[2] = new double[in_num] {1, 3, 2};
+	in_data[0] = new double[in_num] {0, 0};
+	in_data[1] = new double[in_num] {0, 1};
+	in_data[2] = new double[in_num] {1, 0};
+	in_data[3] = new double[in_num] {1, 1};
 	double** bench_data = new double*[batch_num];
-	bench_data[0] = new double[out_num] {0.30, 0.50};
-	bench_data[1] = new double[out_num] {0.50, 0.80};
-	bench_data[2] = new double[out_num] {0.60, 0.90};
+	bench_data[0] = new double[out_num] {0};
+	bench_data[1] = new double[out_num] {0};
+	bench_data[2] = new double[out_num] {0};
+	bench_data[3] = new double[out_num] {1};
 	std::cout << "input:" << std::endl;
 	for (int i = 0; i < batch_num; i++) {
 		for (int j = 0; j < in_num; j++) {
@@ -55,11 +57,11 @@ int main()
 	int run_num = 0;
 	double** out_data;
 	double** loss_data;
-	while (mse >= 0.001 && run_num < 100) {
+	while (mse >= 0.001 && run_num < 1000) {
 		//forward
 		out_data = fc.forward(in_data);
 		out_data = fc1.forward(out_data);
-/*		out_data = fc2.forward(out_data);*/
+		out_data = fc2.forward(out_data);
 		std::cout << "output:" << std::endl;
 		for (int i = 0; i < batch_num; i++) {
 			for (int j = 0; j < out_num; j++) {
@@ -72,7 +74,7 @@ int main()
 		mse = loss.forward(out_data, bench_data, out_num, batch_num);
 		std::cout << "mse = " << mse << ";" << std::endl;
 		loss_data = loss.backward();
-/*		loss_data = fc2.backward(loss_data);*/
+		loss_data = fc2.backward(loss_data);
 		loss_data = fc1.backward(loss_data);
 		loss_data = fc.backward(loss_data);
 		run_num++;
